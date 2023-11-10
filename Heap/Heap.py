4f992e536ffce820897a9f9eed295467dcf97378
navaneethbr <navaneethbr2001@gmail.com>
2023-11-10 11:53:34 +0530

    added both min heap and max heap

--- Heap/Heap.py
+++ Heap/Heap.py
@@ -56,7 +56,35 @@ class MinHeap:
         
     def display(self):
         print(*self.mHeap)
-             
+
+class MaxHeap():
+    def __init__(self):
+        self.Heap = []
+        
+    def __parent(self, idx):
+        return idx//2
+    
+    def __leftchild(self, idx):
+        return (2*idx)
+    
+    def __rightchild(self, idx):
+        return (2*idx)+1
+
+    def __swap(self, i, j):
+        self.Heap[i-1], self.Heap[j-1] = self.Heap[j-1], self.Heap[i-1]
+    
+    def __getValue(self):
+        print("Enter the number to be inserted")
+        return int(input()) 
+
+    def insert(self):
+        value = self.__getValue()
+        self.Heap.append(value)
+        l = len(self.Heap)
+        while self.__parent(l)-1 >= 0 and self.Heap[self.__parent(l)-1] < self.Heap[l-1]:
+            self.__swap(l, self.__parent(l))
+            l = self.__parent(l)
+         
 if __name__ == "__main__":
     o = MinHeap()
     insert = True
